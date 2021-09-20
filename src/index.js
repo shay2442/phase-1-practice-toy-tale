aa34c5761553095d1f9799bb82d0db20efda7a87
Shay Wacker <shay2442@yahoo.com>
2021-09-20 17:14:29 -0500

    Working on post

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 
 let addToy = false;
 let toyCollectionDiv = document.querySelector('div#toy-collection')
-let toyForm = document.querySelector('')
-console.log(toyCollectionDiv)
+let toyContainer = document.querySelector('.container')
+let toyForm = document.querySelector('.add-toy-form')
+let newToyButton = document.querySelector('#new-toy-btn')
+// console.log(newToyButton)
+// console.log(toyContainer)
+// console.log(toyForm)
 
 const BASE_URL = "http://localhost:3000/toys"
 
@@ -43,7 +47,7 @@ fetch(BASE_URL)
     likesButton.addEventListener('click', () => increaseLikes(toy, likesNum))
 
     function increaseLikes(toy, likesNum) {
-      ++toy.likes 
+      toy.likes++
       likesNum.textContent = `${toy.likes} Likes`
       console.log(likesNum)
 
@@ -53,19 +57,74 @@ fetch(BASE_URL)
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({likes: toy.likes + 1})
+        body: JSON.stringify({likes: toy.likes})
       })
       .then(res => res.json())
       .then(function(updatedToyObj){
         toy.likes = updatedToyObj.likes
+
       })
     
+    } //increaseLikes function ends here 
+
+  } //createCard ends here
+
+  toyForm.addEventListener('submit', (event) => {
+    createToy(event)
+  })
+
+  function createToy(event) {
+    event.preventDefault()
+    let name = e.target.name.value
+    let image = e.target.image.value
+    
+    console.log(name)
+    console.log(image)
+  
+
+ 
+
+    const newToy = {
+      name : name,
+      image : image,
+      likes : 0
+
     }
-
-
+    
   }
+    createToy()
 
-
+  // function createItem() {
+  //   event.preventDefault()
+  //   // This event handler should create a new Items object and persist data
+  //   const name = document.querySelector('#items-input').value
+  //   const quantity = document.querySelector('#quantity-input').value
+  
+  //   // event.target['items-input'].value
+  //   // +event.target["quantity-input"].value
+  
+  //   // Create an items object
+  //   const newObj = {
+  //     name: name, 
+  //     quantity: quantity,
+  //     likes: 0,
+  //     completed: false
+  //   }
+  
+  //   // Persist this data
+  
+  //   fetch(BASE_URL, {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify(newObj)
+  //   })
+  //   .then(resp => resp.json())
+  //   .then(item => renderItem(item))
+  //   // .then(renderItem) // refactored line 41
+    
+  //   itemsForm.reset()
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
